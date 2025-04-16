5d22f47be56c87163a62eab82cd75bcd77878aa8
Stephen Williams <stephwi3@cisco.com>
2025-04-16 11:34:12 -0700

    adding browserslist and regenerator-runtime, adjusting index.html

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 // import polyfill to support older browswers
 import '@webcomponents/custom-elements';
+import 'regenerator-runtime/runtime.js';
 // import custom element for external use
 import './FormBuilderElement';
 
@@ -7,22 +8,3 @@ export { FormBuilderElement } from './FormBuilderElement';
 
 // Re-export types for external use
 export type { FormBuilderConfig, ShowErrorListOptions } from './types/shared';
-
-// * * * * IF INSTANTIATING AS NORMAL REACT COMPONENT * * * *
-
-// import React from 'react';
-
-// const FormBuilder = () => {
-//     return (
-//         <div id="component-container">
-//             <form-builder
-//                 oidc-url="some-oidc-url"
-//                 fbms-base-url="/some-fbms-url"
-//                 fbms-form-fname="Some Form Name"
-//                 styles="form {max-width: 500px}"
-//             ></form-builder>
-//         </div>
-//     );
-// };
-
-// export default FormBuilder;
